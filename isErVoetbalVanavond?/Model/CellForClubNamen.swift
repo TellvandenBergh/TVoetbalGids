8703ff95386081a0e282c702745f5a2c82b18d6f
Tell van den Bergh <tellvandenbergh@gmail.com>
2019-07-03 15:43:12 +0200

    commit 1

---
+++ isErVoetbalVanavond?/Model/CellForClubNamen.swift
@@ -0,0 +1,39 @@
+//
+//  CellForClubNamen.swift
+//  isErVoetbalVanavond?
+//
+//  Created by Tell van den Bergh on 14/02/2019.
+//  Copyright © 2019 Tell van den Bergh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CellForClubNamen: UITableViewCell {
+    
+    
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    
+    
+    
+    func Myinit(imageName: String, labelName: String) {
+        
+        imageOutlet.image = UIImage(named: imageName)
+        labelOutlet.text = labelName
+    
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        visualEffectView.layer.cornerRadius = 20
+        visualEffectView.clipsToBounds = true
+        visualEffectView.alpha = 0.75
+        labelOutlet.adjustsFontSizeToFitWidth = true
+        
+    }
+    
+}
